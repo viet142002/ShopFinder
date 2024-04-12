20146e270d7caadc2edd8cc5c6fd2807554cc77f
QuocViet <vietnguyen142002@gmail.com>
2024-04-12 22:46:39 +0700

    update add timetamps to retailerModel and informationModel

--- Server/src/Models/informationModel.js
+++ Server/src/Models/informationModel.js
@@ -1,42 +1,47 @@
 const mongoose = require('mongoose');
 
-const informationSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: [true, 'Store name already exists'],
-    },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-    },
-    location: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Location',
-    },
-    type: {
-        type: String,
-        required: true,
-    },
-    phone: {
-        type: String,
-        default: '',
-    },
-    description: {
-        type: String,
-        default: '',
-    },
-    logo: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Image',
-        default: '65fd4951f687ef962765aa4a',
-    },
-    images: [
-        {
+const informationSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: [true, 'Store name already exists'],
+        },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        },
+        location: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Location',
+        },
+        type: {
+            type: String,
+            required: true,
+        },
+        phone: {
+            type: String,
+            default: '',
+        },
+        description: {
+            type: String,
+            default: '',
+        },
+        logo: {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'Image',
+            default: '65fd4951f687ef962765aa4a',
         },
-    ],
-});
+        images: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Image',
+            },
+        ],
+    },
+    {
+        timestamps: true,
+    }
+);
 
 module.exports = mongoose.model('Information', informationSchema);
